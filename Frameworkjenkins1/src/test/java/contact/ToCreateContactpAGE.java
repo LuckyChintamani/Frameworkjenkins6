a1d8e8ff1546f5023970d895411e03d86b7adbb1
santosh <santosh@DESKTOP-68V5I16>
2024-10-07 21:22:31 +0530

    FremarkCreatedjenkins3

---
+++ Frameworkjenkins1/src/test/java/contact/ToCreateContactpAGE.java
@@ -0,0 +1,9 @@
+package contact;
+
+public class ToCreateContactpAGE {
+	
+	public static void main(String[] args) {
+		System.out.println(" contact page is created ");
+	}
+
+}
---
+++ Frameworkjenkins1/src/test/java/organigation/ToCreateOrganizationPagae.java
@@ -0,0 +1,8 @@
+package organigation;
+
+public class ToCreateOrganizationPagae {
+
+	public static void main(String[] args) {
+	System.out.println("organization page is created");
+
+}
